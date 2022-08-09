ce0f5c9f942f7441eb460c1a4a8662ae6e0c0316
VIGILIO <yonatanvigiliolavado09gmail.com>
2022-08-09 01:36:49 -0500

    new updated

--- public/index.php
+++ public/index.php
@@ -1,14 +1,13 @@
 <?php
 
-require_once __DIR__ . '/../config/app.php';
+require_once __DIR__ . '/../app/app.php';
 
+use App\Router;
 use Controller\UsuarioController;
-use MVC\Router;
 
 
-$router = new Router;
-
-$router->post('/api/addUser', [UsuarioController::class, 'addUser']);
+$router = new Router();
+$router->get('/api/user', [UsuarioController::class, 'addUser']);
 $router->get('/', [UsuarioController::class, 'home']);
 
 $router->comprobarRutas();
